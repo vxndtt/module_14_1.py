bde25bf218f4292de8d71374c1cce3f8f1cf0827
vxndtt <alenakad16@mail.ru>
2025-01-11 14:07:42 +0500

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -21,14 +21,13 @@ for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)', (f'User{i}', f'example{i}@gmail.com', (i*10), 1000))
 
 for i in range(1, 11, 2):
-    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{i}'))
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, i))
 
 for i in range(1, 11, 3):
-    cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+    cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
 
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 users = cursor.fetchall()
 for user in users:
     username, email, age, balance = user
     print(f"Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance}")
-
